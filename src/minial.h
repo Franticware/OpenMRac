8e847a792d0153909f030345d715a374b64f225c
Vojta <vojta.salajka@seznam.cz>
2022-07-15 20:51:30 +0200

    Updated Makefile.linux, reorganized minial.h

--- src/minial.h
+++ src/minial.h
@@ -6,51 +6,51 @@
 struct ALCcontext;
 struct ALCdevice;
 
-typedef unsigned int ALuint;
 typedef char ALCboolean;
 typedef char ALCchar;
 typedef int ALCenum;
 typedef int ALCint;
+typedef unsigned int ALuint;
 
-#define ALC_ALL_DEVICES_SPECIFIER                0x1013
 #define ALC_DEVICE_SPECIFIER                     0x1005
+#define ALC_ALL_DEVICES_SPECIFIER                0x1013
 
-ALCboolean alcIsExtensionPresent(ALCdevice *device, const ALCchar *extname);
-const ALCchar* alcGetString(ALCdevice *device, ALCenum param);
-ALCdevice* alcOpenDevice(const ALCchar *devicename);
-ALCcontext* alcCreateContext(ALCdevice *device, const ALCint *attrlist);
-ALCboolean  alcMakeContextCurrent(ALCcontext *context);
-void alcDestroyContext(ALCcontext *context);
 ALCboolean alcCloseDevice(ALCdevice *device);
+ALCcontext* alcCreateContext(ALCdevice *device, const ALCint *attrlist);
+void alcDestroyContext(ALCcontext *context);
+const ALCchar* alcGetString(ALCdevice *device, ALCenum param);
+ALCboolean alcIsExtensionPresent(ALCdevice *device, const ALCchar *extname);
+ALCboolean  alcMakeContextCurrent(ALCcontext *context);
+ALCdevice* alcOpenDevice(const ALCchar *devicename);
 
-typedef float ALfloat;
 typedef int ALenum;
+typedef float ALfloat;
+typedef int ALint;
 typedef int ALsizei;
 typedef unsigned int ALuint;
 typedef void ALvoid;
-typedef int ALint;
 
+#define AL_PITCH                                 0x1003
 #define AL_POSITION                              0x1004
 #define AL_VELOCITY                              0x1006
-#define AL_ORIENTATION                           0x100F
-#define AL_FORMAT_MONO16                         0x1101
-#define AL_PITCH                                 0x1003
-#define AL_GAIN                                  0x100A
 #define AL_LOOPING                               0x1007
 #define AL_BUFFER                                0x1009
+#define AL_GAIN                                  0x100A
+#define AL_ORIENTATION                           0x100F
 #define AL_SAMPLE_OFFSET                         0x1025
+#define AL_FORMAT_MONO16                         0x1101
 
-void alListenerfv(ALenum param, const ALfloat *values);
-void alDeleteSources(ALsizei n, const ALuint *sources);
-void alDeleteBuffers(ALsizei n, const ALuint *buffers);
-void alGenBuffers(ALsizei n, ALuint *buffers);
 void alBufferData(ALuint buffer, ALenum format, const ALvoid *data, ALsizei size, ALsizei freq);
+void alDeleteBuffers(ALsizei n, const ALuint *buffers);
+void alDeleteSources(ALsizei n, const ALuint *sources);
+void alGenBuffers(ALsizei n, ALuint *buffers);
 void alGenSources(ALsizei n, ALuint *sources);
+void alListenerfv(ALenum param, const ALfloat *values);
 void alSourcef(ALuint source, ALenum param, ALfloat value);
 void alSourcefv(ALuint source, ALenum param, const ALfloat *values);
 void alSourcei(ALuint source, ALenum param, ALint value);
 void alSourcePlay(ALuint source);
-void alSourceStop(ALuint source);
 void alSourceRewind(ALuint source);
+void alSourceStop(ALuint source);
 
 #endif // MINIAL_H
