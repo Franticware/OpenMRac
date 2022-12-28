cffbd39f25bb30629ea9072cf356afc2e8d4b343
Vojta <vojta.salajka@seznam.cz>
2022-12-29 00:42:04 +0100

    Added SDL2 to prereq.sh

--- src/mingw32/prereq.sh
+++ src/mingw32/prereq.sh
@@ -3,22 +3,24 @@
 cd ..
 
 : <<'END'
-
 END
+
+SDL2VER=2.26.1
+rm -Rf SDL2
+rm -f SDL2-devel-$SDL2VER-mingw.tar.gz SDL2.dll
+wget https://libsdl.org/release/SDL2-devel-$SDL2VER-mingw.tar.gz
+tar -xvf SDL2-devel-$SDL2VER-mingw.tar.gz
+mv SDL2-$SDL2VER SDL2
+cp SDL2/i686-w64-mingw32/bin/SDL2.dll .
 
 rm -Rf libjpeg-turbo
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
-
 cd libjpeg-turbo
-
 cmake -DCMAKE_TOOLCHAIN_FILE="../mingw32/mingw32-toolchain.cmake" \
 -S. -B. \
 -DCMAKE_GNUtoMS:BOOL="0" -DCMAKE_BUILD_TYPE:STRING="MinSizeRel" -DCMAKE_INSTALL_PREFIX:PATH="." -DCMAKE_SYSTEM_PROCESSOR:STRING="" -DWITH_ARITH_DEC:BOOL="0" -DCMAKE_INSTALL_LIBDIR:PATH="lib" -DWITH_SIMD:BOOL="1" -DWITH_TURBOJPEG:BOOL="0" -DPKGNAME:STRING="libjpeg-turbo" -DBUILD:STRING="20221228" -DWITH_ARITH_ENC:BOOL="0" -DENABLE_SHARED:BOOL="0" 
 make
-
 cd ..
-
-
 
 rm -Rf zlib
 git clone https://github.com/madler/zlib
@@ -27,7 +29,6 @@ cmake -DCMAKE_TOOLCHAIN_FILE="../mingw32/mingw32-toolchain.cmake" \
 -S. -B. \
 -DLIBRARY_OUTPUT_PATH:PATH="" -DINSTALL_LIB_DIR:PATH="/usr/local/lib" -DCMAKE_BUILD_TYPE:STRING="MinSizeRel" -DCMAKE_INSTALL_PREFIX:PATH="/usr/local" -DINSTALL_MAN_DIR:PATH="/usr/local/share/man" -DINSTALL_PKGCONFIG_DIR:PATH="/usr/local/share/pkgconfig" -DEXECUTABLE_OUTPUT_PATH:PATH="" 
 make
-
 cd ..
 
 rm -Rf libpng
@@ -37,5 +38,4 @@ cmake -DCMAKE_TOOLCHAIN_FILE="../mingw32/mingw32-toolchain.cmake" \
 -S. -B. \
 -DZLIB_INCLUDE_DIRS:PATH="../zlib" -DPNG_SHARED:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="/usr/local" -DCMAKE_BUILD_TYPE:STRING="MinSizeRel" -DPNG_PREFIX:STRING="" -DPNG_BUILD_ZLIB:BOOL="1" 
 make
-
 cd ..
